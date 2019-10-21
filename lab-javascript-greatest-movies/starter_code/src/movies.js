ed389814cba91151289fe49a171bea72d409c3b0
mjarraya <mjarraya@student.42.fr>
2019-10-21 11:53:26 +0200

    movies lab done

--- lab-javascript-greatest-movies/starter_code/src/movies.js
+++ lab-javascript-greatest-movies/starter_code/src/movies.js
@@ -52,24 +52,177 @@ function orderByYear(movies) {
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 function howManyMovies(movies) {
-  let spielbergMovies = movies.filter(function(movie) {
-    // return (
-    //   movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
-    // );
+  //   let spielbergMovies = movies.filter(function(movie) {
+  //     // return (
+  //     //   movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+  //     // );
 
+  //     if (
+  //       movie.director === "Steven Spielberg" &&
+  //       movie.genre.includes("Drama")
+  //     ) {
+  //       return true;
+  //     }
+  //   });
+
+  //   return spielbergMovies.length;
+
+  return movies.reduce(function(accumulator, value) {
     if (
-      movie.director === "Steven Spielberg" &&
-      movie.genre.includes("Drama")
+      value.director === "Steven Spielberg" &&
+      value.genre.includes("Drama")
     ) {
-      return true;
+      accumulator += 1;
     }
-  });
-
-  return spielbergMovies.length;
+    return accumulator;
+  }, 0);
 }
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(movies) {
+  //   const movieTitles = movies.map(function(movie) {
+  //     return movie.title;
+  //   });
+
+  //   movieTitles.sort(function(a, b) {
+  //     return a.localeCompare(b);
+  //   });
+
+  //   return movieTitles.splice(0, 20);
+
+  const movieTitles = movies
+    .map(function(movie) {
+      return movie.title;
+    })
+    .sort(function(a, b) {
+      return a.localeCompare(b);
+    })
+    .splice(0, 20);
+
+  return movieTitles;
+}
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
+function convertDuration(stringDuration) {
+  const hourIndex = stringDuration.indexOf("h");
+
+  let hours = 0;
+  if (hourIndex >= 0) {
+    hours = parseInt(stringDuration.slice(0, hourIndex));
+  }
+
+  let minutes = 0;
+  if (stringDuration.includes("min")) {
+    minutes = parseInt(stringDuration.split(" ").pop());
+  }
+
+  return hours * 60 + minutes;
+}
+
+function turnHoursToMinutes(movies) {
+  return movies.map(function(movie) {
+    const newDuration = convertDuration(movie.duration);
+
+    return {
+      ...movie,
+      duration: newDuration
+    };
+
+    // return Object.assign({}, movie, { duration: newDuration });
+
+    // return {
+    //     title: movie.title,
+    //     year: movie.year,
+    //     director: movie.director,
+    //     genre: movie.genre,
+    //     rate: movie.rate,
+    //     duration: newDuration
+    // };
+  });
+}
+
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+function bestYearAvg(movies) {
+  if (!movies.length) {
+    return null;
+  }
+
+  const years = movies.map(function(movie) {
+    return movie.year;
+  });
+  const uniqueYears = years
+    .filter(function(year, index) {
+      if (years.indexOf(year) === index) {
+        return true;
+      }
+    })
+    .sort(function(a, b) {
+      return a - b;
+    });
+
+  //   let topYear;
+  //   let topAvg;
+
+  //   for (const year of uniqueYears) {
+  //     const moviesForYear = movies.filter(function(movie) {
+  //       if (movie.year === year) {
+  //         return true;
+  //       }
+  //     });
+  //     const averageForYear = ratesAverage(moviesForYear);
+  //     if (!topAvg || averageForYear > topAvg) {
+  //       topAvg = averageForYear;
+  //       topYear = year;
+  //     }
+  //   }
+
+  const top = uniqueYears.reduce(
+    function(accumulator, year) {
+      const moviesForYear = movies.filter(function(movie) {
+        if (movie.year === year) {
+          return true;
+        }
+      });
+      const averageForYear = ratesAverage(moviesForYear);
+      if (!accumulator.avg || averageForYear > accumulator.avg) {
+        accumulator.avg = averageForYear;
+        accumulator.year = year;
+      }
+
+      return accumulator;
+    },
+    { year: null, avg: null }
+  );
+
+  return (
+    "The best year was " + top.year + " with an average rate of " + top.avg
+  );
+}
+
+// function bestYearAvg(movies) {
+//   if (!movies.length) return null;
+
+//   const yearAvg = movies.reduce(function(accumulator, value) {
+//     if (!accumulator[value.year]) {
+//       accumulator[value.year] = Number(value.rate);
+//     } else {
+//       accumulator[value.year] += Number(value.rate);
+//       accumulator[value.year] /= 2;
+//     }
+//     return accumulator;
+//   }, {});
+
+//   const sortedByRating = Object.entries(yearAvg).sort(function(a, b) {
+//     return b[1] - a[1];
+//   });
+
+//   const topRating = sortedByRating[0][1];
+//   const topYear = sortedByRating[0][0];
+
+//   return (
+//     "The best year was " + topYear + " with an average rate of " + topRating
+//   );
+// }
